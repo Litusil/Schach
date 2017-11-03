a03acbfd6bf660c07f7502a92cc190be7769c302
ma721kup@FHKN.DE <ma721kup@FHKN.DE>
2017-11-03 10:26:02 +0100

    print aus main eingefügt

--- src/main/scala/ChessBoard.scala
+++ src/main/scala/ChessBoard.scala
@@ -8,4 +8,17 @@ object ChessBoard {
     Char
       order(1)
   }
+
+  def print(): Unit ={
+    for(i <- 0 to 7){
+      for(j <- 0 to 7) {
+        if(ChessBoard.board(i)(j) != null) {
+          System.out.format("%3s", ChessBoard.board(i)(j).toString)
+        } else {
+          System.out.format("%3s", "X")
+        }
+      }
+      System.out.println("\n")
+    }
+  }
 }
