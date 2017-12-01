31bd1dbee3de48bd72c81e0a3c0a2fac2ff86964
lcapellino <lucas_capellino@web.de>
2017-12-01 09:25:27 +0100

    Pawn moveset added

--- src/main/scala/model/Pawn.scala
+++ src/main/scala/model/Pawn.scala
@@ -6,26 +6,42 @@ class Pawn(color : Boolean) extends ChessPiece(color) {
 
   override def getPossibleMoves(chessBoard: Array[Array[ChessPiece]]): Vector[(Int, Int)] = {
     val pos = this.getPosition(chessBoard)
-    val possibleMoves: Vector[(Int,Int)] = Vector()
-    var xIncrementer = 1;
-    // TODO: IMPLEMENT!
+    val possibleMoves: Vector[(Int, Int)] = Vector()
+    var yIncrementer = 1;
 
-    if(this.hasMoved == false){
-      if(this.color){
-        xIncrementer = 2
-      } else {
-        xIncrementer = -2
-      }
-
-
+    if (!this.color) {
+      yIncrementer = -1
     }
 
+    if (chessBoard.length > pos._1 + yIncrementer){
+      possibleMoves :+ (pos._1 + yIncrementer, pos._2)
+    } else {
+      return possibleMoves
+    }
+    if(chessBoard(pos._1 + yIncrementer)(pos._2 + 1) != null) {
+      if (chessBoard(pos._1 + yIncrementer)(pos._2 + 1).color != this.color) {
+        possibleMoves :+ chessBoard(pos._1 + yIncrementer)(pos._2 + 1)
+      }
+    }
+    if(chessBoard(pos._1 + yIncrementer)(pos._2 - 1) != null) {
+      if (chessBoard(pos._1 + yIncrementer)(pos._2 - 1).color != this.color) {
+        possibleMoves :+ chessBoard(pos._1 + yIncrementer)(pos._2 - 1)
+      }
+    }
 
-
+    if(!this.hasMoved) {
+      if (this.color) {
+        yIncrementer = 2
+      } else {
+        yIncrementer = -2
+      }
+      if (chessBoard(pos._1 + yIncrementer)(pos._2) == null){
+        possibleMoves :+ (pos._1 + yIncrementer, pos._2)
+      }
+    }
     possibleMoves
-
-
   }
+
 
   override def toString(): String ={
     if (color) {
--- src/test/scala/PawnSpec.scala
+++ src/test/scala/PawnSpec.scala
@@ -1,7 +1,43 @@
-import model.Pawn
+import model.{Bishop, ChessBoardFactory, Pawn}
 import org.specs2.mutable._
 
 class PawnSpec extends Specification{
+
+  "A Pawn" should{
+    var pawn = new Pawn(true)
+    var chessBoard = new ChessBoardFactory().create(3)
+    "not be on chessboard" in {
+      pawn.getPosition(chessBoard) must be_== ((-1,-1))
+    }
+  }
+
+  "A Pawn" should{
+    var pawn = new Pawn(true)
+    var chessBoard = new ChessBoardFactory().create(3)
+    var moveableFields: Vector[(Int,Int)] = Vector();
+    chessBoard (2)(1) = pawn
+
+    "have no possible moves on chessboard" in {
+      pawn.getPossibleMoves(chessBoard) must be_== (moveableFields)
+    }
+  }
+
+  "A Pawn" should{
+    var pawn = new Pawn(true)
+    var chessBoard = new ChessBoardFactory().create(3)
+    var moveableFields: Vector[(Int,Int)] = Vector();
+    chessBoard (0)(1) = pawn
+    chessBoard (1)(0) = new Pawn(false)
+    chessBoard (1)(2) = new Pawn(true)
+
+    moveableFields :+ (1,1)
+    moveableFields :+ (2,1)
+    moveableFields :+ (1,0)
+    "have possible moves on chessboard" in {
+      pawn.getPossibleMoves(chessBoard) must be_== (moveableFields)
+    }
+  }
+
 
   "A white model.Pawn" should {
     val r = new Pawn(true)
@@ -18,8 +54,5 @@ class PawnSpec extends Specification{
       r.toString must be_== ("\u265F")
     }
   }
-
-
-
 }
 
