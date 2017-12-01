c414eec25c2a873b6eb0c55fc49442734af8c62a
lcapellino <lucas_capellino@web.de>
2017-12-01 09:33:52 +0100

    Queen moveset added

--- src/main/scala/model/Queen.scala
+++ src/main/scala/model/Queen.scala
@@ -1,10 +1,21 @@
 package model
 
+import scala.collection.immutable.Vector
+
 class Queen(color : Boolean) extends ChessPiece(color) {
 
   override def getPossibleMoves(chessBoard: Array[Array[ChessPiece]]): Vector[(Int, Int)] = {
-    //not implemented
-    return Vector((0,0))
+    val pos = this.getPosition(chessBoard)
+    val possibleMoves: Vector[(Int,Int)] = Vector()
+    possibleMoves :+ MoveSetUtil.getSelectableFields(pos._2, pos._1, Direction.LEFT,chessBoard)
+    possibleMoves :+ MoveSetUtil.getSelectableFields(pos._2, pos._1, Direction.RIGHT,chessBoard)
+    possibleMoves :+ MoveSetUtil.getSelectableFields(pos._2, pos._1, Direction.UP,chessBoard)
+    possibleMoves :+ MoveSetUtil.getSelectableFields(pos._2, pos._1, Direction.DOWN,chessBoard)
+    possibleMoves :+ MoveSetUtil.getSelectableFields(pos._2, pos._1, Direction.LEFT_UP,chessBoard)
+    possibleMoves :+ MoveSetUtil.getSelectableFields(pos._2, pos._1, Direction.RIGHT_UP,chessBoard)
+    possibleMoves :+ MoveSetUtil.getSelectableFields(pos._2, pos._1, Direction.LEFT_DOWN,chessBoard)
+    possibleMoves :+ MoveSetUtil.getSelectableFields(pos._2, pos._1, Direction.RIGHT_DOWN,chessBoard)
+    possibleMoves
   }
 
   override def toString(): String ={
--- src/test/scala/QueenSpec.scala
+++ src/test/scala/QueenSpec.scala
@@ -1,7 +1,49 @@
-import model.Queen
+import model.{ChessBoardFactory, Queen}
 import org.specs2.mutable._
 
 class QueenSpec extends Specification{
+
+
+  "A Queen" should{
+    var queen = new Queen(true)
+    var chessBoard = new ChessBoardFactory().create(3)
+    "not be on chessboard" in {
+      queen.getPosition(chessBoard) must be_== ((-1,-1))
+    }
+  }
+
+  "A Queen" should{
+    var queen = new Queen(true)
+    var chessBoard = new ChessBoardFactory().create(2)
+    chessBoard (1)(1) = queen
+    chessBoard (0)(1) = new Queen(true)
+    chessBoard (1)(0) = new Queen(true)
+    chessBoard (0)(0) = new Queen(true)
+    var moveableFields: Vector[(Int,Int)] = Vector();
+    "has no possible moves on chessboard" in {
+      queen.getPossibleMoves(chessBoard) must be_== (moveableFields)
+    }
+  }
+
+  "A Queen" should{
+    var queen = new Queen(true)
+    var chessBoard = new ChessBoardFactory().create(3)
+    chessBoard (1)(1) = queen
+    chessBoard (0)(1) = new Queen(true)
+    chessBoard (1)(0) = new Queen(false)
+    var moveableFields: Vector[(Int,Int)] = Vector();
+    moveableFields :+ (1,0)
+    moveableFields :+ (2,1)
+    moveableFields :+ (1,2)
+    moveableFields :+ (1,2)
+    moveableFields :+ (0,2)
+    moveableFields :+ (2,2)
+    moveableFields :+ (0,0)
+    "have possible moves on chessboard" in {
+      queen.getPossibleMoves(chessBoard) must be_== (moveableFields)
+    }
+  }
+
 
   "A white model.Queen" should {
     val r = new Queen(true)
