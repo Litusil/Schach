b73f1837bbec465f8435c5dc0ce9ad76cdbeb702
ma721kup <kupferschmidmathias@gmail.com>
2018-01-19 11:31:58 +0100

    Anpassungen damit in XML gespeichert werden kann

--- src/test/scala/model/BishopSpec.scala
+++ src/test/scala/model/BishopSpec.scala
@@ -7,7 +7,7 @@ class BishopSpec extends Specification{
 
 
   "A Bishop" should{
-    var r = new Bishop(true)
+    var r = new Bishop(true,false)
     var chessBoard = new ChessBoardFactory().create(3)
     "not be on chessboard" in {
       r.getPosition(chessBoard) must be_== ((-1,-1))
@@ -15,11 +15,11 @@ class BishopSpec extends Specification{
   }
 
   "A Bishop" should{
-    var r = new Bishop(true)
+    var r = new Bishop(true,false)
     var chessBoard = new ChessBoardFactory().create(3)
     chessBoard (1)(1) = r
-    chessBoard (0)(0) = new Bishop(true)
-    chessBoard (2)(2) = new Bishop(false)
+    chessBoard (0)(0) = new Bishop(true,false)
+    chessBoard (2)(2) = new Bishop(false,false)
     var moveableFields: Vector[(Int,Int)] = Vector()
     moveableFields = moveableFields :+ (2,0)
     moveableFields = moveableFields :+ (2,2)
@@ -30,14 +30,14 @@ class BishopSpec extends Specification{
   }
 
   "A white model.Bishop" should {
-    val r = new Bishop(true)
+    val r = new Bishop(true,false)
     "have toString() that is \u2657" in {
       r.toString must be_== ("\u2657")
     }
   }
 
   "A black model.Bishop" should {
-    val r = new Bishop(false)
+    val r = new Bishop(false,false)
     "have toString that is \u265D" in {
       r.toString must be_== ("\u265D")
     }
